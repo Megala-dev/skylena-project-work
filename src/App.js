489da00f4337756734f54643d5428ae77623fafb
Megala-dev <megala77922@gmail.com>
2025-06-11 16:56:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import myImage from './image1/image1.jpeg';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="main-heading"> Welcome to My Static Page</h1>
+        <p className="sub-heading">This is a simple frontend created using React.</p>
+         <div className="card">
+           <h2> About Me</h2>
+           <p>I am interested in coding. I like to create websites using HTML, CSS, and REACT. I want to become a Full Stack Developer.</p>
+         </div>
+        
+    </div>
+  );
+}
+
+export default App;
